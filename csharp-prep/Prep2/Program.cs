e3a5ea25ee4cecc77b087fe45571691f39d7b389
Austin Hicken <austin.t.hicken@gmail.com>
2026-01-15 19:58:00 -0700

    Made a program that takes a decimal percentage and calculates what letter grade you got, including "+" and "-" signs for letter grades i.e. A- or D+

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string ValueInText = Console.ReadLine();
+        double Number = double.Parse(ValueInText);
+        string Letter = "";
+        string Sign = "";
+
+        double Remainder = Number % 10;
+        if (Remainder >= 7)
+        {
+            Sign = "+";
+        }
+        else if (Remainder < 3)
+        {
+            Sign = "-";
+        }
+
+        if (Number >= 90)
+        {
+            Letter = "A";
+        }
+        else if (Number >= 80)
+        {
+            Letter = "B";
+        }
+        else if (Number >= 70)
+        {
+            Letter = "C";
+        }
+        else if (Number >= 60)
+        {
+            Letter = "D";
+        }
+        else if (Number < 60)
+        {
+            Letter = "F";
+        }
+
+        if (Letter == "A" && Remainder >= 7)
+        {
+            Sign = "";
+        }
+        else if (Letter == "F")
+        {
+            Sign = "";
+        }
+
+        if (Number >= 70)
+        {
+            Console.Write($"Hooray! You passed with a {Letter}{Sign} at {Number}%!");
+        }
+        else if (Number <= 70)
+        {
+            Console.Write($"Sorry, you failed with a {Letter}{Sign} at {Number}%... Better luck next time!");
+        }
     }
 }
