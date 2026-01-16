8e906a0256b0d416b2883b2bdb2e084eb9bcf2b3
Austin Hicken <austin.t.hicken@gmail.com>
2026-01-16 01:47:55 -0700

    Completed the fifth C# prep assignment; code comprised of functions prompts the user for their name, favorite number, and birth year, then tells them the square of their favorite number, and how old they will be this year.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber();
+        int SquaredNumber = SquareNumber(UserNumber);
+        int BirthYear;
+        PromptUserBirthYear(out BirthYear);
+        DisplayResult(UserName, SquaredNumber, BirthYear);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string Name = Console.ReadLine();
+        return Name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int Number = int.Parse(Console.ReadLine());
+        return Number;
+    }
+    
+    static void PromptUserBirthYear(out int BirthYear)
+    {
+        Console.Write($"Please enter the year you were born: ");
+        BirthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int Number)
+    {
+        int Square = Number * Number;
+        return Square;
+    }
+
+    static void DisplayResult(string Name, int Square, int BirthYear)
+    {
+        Console.WriteLine($"{Name}, the square of your favorite number is {Square}.");
+        Console.WriteLine($"{Name}, you will turn {2026 - BirthYear} years old this year.");
     }
 }
