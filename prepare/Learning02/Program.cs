c823e9911003463796787d0822c92aab298d6938
Austin Hicken <austin.t.hicken@gmail.com>
2026-01-22 20:48:17 -0700

    Created a job resume creator using classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Lee's Marketplace";
+        job1._jobTitle = "Cashier";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Intermountain Healthcare";
+        job2._jobTitle = "Medical Assistant - Dermatology";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Job job3 = new Job();
+        job3._company = "Intermountain Healthcare";
+        job3._jobTitle = "Limited X-ray Tech";
+        job3._startYear = 2023;
+        job3._endYear = 2024;
+
+        Resume resume = new Resume();
+
+        resume._name = "Austin Hicken";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume._jobs.Add(job3);
+
+        resume.Display();
     }
 }
